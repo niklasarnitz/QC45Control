efef1c91a062bfab131b38cfa16acc13bf7b5b67
Niklas Arnitz <niklasarnitz@icloud.com>
2023-02-01 18:03:39 +0100

    Initial Commit

---
+++ QC45Control/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  QC45Control
+//
+//  Created by Niklas Arnitz on 01.02.23.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
